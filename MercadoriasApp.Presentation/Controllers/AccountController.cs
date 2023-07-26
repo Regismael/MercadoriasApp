71d66b8502523bbc488ac284d84d49ef3d727725
regismael <ismael.l.regis@gmail.com>
2023-07-26 18:19:11 -0300

    feat: Desenvolvimento FrontEnd das páginas Login, Register e ForgotPassword

--- MercadoriasApp.Presentation/Controllers/AccountController.cs
+++ MercadoriasApp.Presentation/Controllers/AccountController.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
-namespace MercadoriasApp.Presentation.Controllers
+namespace ContasApp.Presentation.Controllers
 {
+    /// <summary>
+    /// Classe de controle do Asp.Net MVC
+    /// </summary>
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        /// <summary>
+        /// Método para abrir a página /Account/Login
+        /// </summary>
+        public IActionResult Login()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Método para abrir a página /Account/Register
+        /// </summary>
+        public IActionResult Register()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Método para abrir a página /Account/ForgotPassword
+        /// </summary>
+        public IActionResult ForgotPassword()
         {
             return View();
         }
