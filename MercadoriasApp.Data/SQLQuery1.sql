32ea80186790bb25f3cc91a92232dcda6e75b404
regismael <ismael.l.regis@gmail.com>
2023-07-26 18:44:08 -0300

    feat: Desenvolvimento do banco de dados SQL server BDMercadoriasApp

---
+++ MercadoriasApp.Data/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿-- criando a tabela de clientes
+CREATE TABLE CLIENTE(
+ID                 UNIQUEIDENTIFIER       NOT NULL,
+NOME               NVARCHAR(150)          NOT NULL,
+EMAIL              NVARCHAR(100)          NOT NULL UNIQUE,
+SENHA              NVARCHAR(40)           NOT NULL,
+DATAHORACRIACAO    DATETIME               NOT NULL,
+PRIMARY KEY(ID))
+GO
